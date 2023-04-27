0351fc38ba5a4ffbba7ed4a614043daadeb6a076
Alexis Rodriguez <avrodzz@gmail.com>
2023-04-26 21:32:24 -0700

    Finished the BMI-Calculator project.

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -43,6 +43,8 @@ class CalculateViewController: UIViewController {
         if segue.identifier == "goToResult" {
             let destinationVC = segue.destination as! ResultViewController //as! forces the type
             destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
         }
     }
 }
--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -6,17 +6,34 @@
 //  Copyright © 2023 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmi: Float! = 0.0
+    var bmi: BMI?
     
     mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / (height * height)
+        let bmiValue = weight / (height * height)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor.blue)
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle.", color: UIColor.green)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor.red)
+        }
     }
     
     func getBMIValue() -> String {
-        return String(format: "%.1f", bmi)
+        return String(format: "%.1f", bmi?.value ?? 0.0)
     }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No Advice."
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.white
+    }
+    
 }
